bebf878d502334ccec135269c019ce24c9ad600b
myh1000 <myh1000@gmail.com>
2016-11-16 13:56:05 -0800

    canvas will resize base on screen size/device

--- boss_core.js
+++ boss_core.js
@@ -8,6 +8,6 @@ function BossCore(x, y, health)
 
     this.update = function()
     {
-        engine.drawHealth(10, 10, bosscore.health/bosscore.maxhealth * 500, 20, 0xFF0000);
+        engine.drawHealth(10, 10, bosscore.health/bosscore.maxhealth * renderer.width-20, renderer.height/42, 0xFF0000);
     }
 }
